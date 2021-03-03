848b16b7bd2f0bb610f4d2d26650806b70e350e9
filippopalmucci <75141552+filippopalmucci@users.noreply.github.com>
2021-03-03 12:48:40 +0100

    Add files via upload

---
+++ sommasotto50.cpp
@@ -0,0 +1,23 @@
+//somma dei numeri inseriti, se minori di 50.
+#include <iostream>
+using namespace std;
+
+int main () {
+    float numeri = 0;
+    float numero = 0;
+    float somma = 0;
+    int conta = 0;
+
+    cout << "Quanti numeri vuoi inserire?" << endl;
+    cin >> numeri;
+    while (conta < numeri) {
+        cout << "Inserire il numero :" << endl;
+        cin >> numero;
+        if (numero < 50) {
+            somma = somma + numero;
+        }
+        conta++;
+    }
+    cout << "La somma totale e':" << endl;
+    cout << somma << endl;
+}
